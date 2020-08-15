45bfce85a84b18d105381ccd5f0715ba27ba468f
Emmanuel George <rustydcoder@gmail.com>
2020-08-15 22:43:03 +0100

    added UI buttons and proper song selection

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 const fillBar = document.getElementById("fill"),
-   pausePlayBtn = document.querySelectorAll('.play img'),
+   buttons = document.querySelectorAll('.play'),
    songPoster = document.querySelector('#image img'),
    bgImg = document.querySelector('#bg img');
+
 
 const songs = ["Song1.mp3", "Song2.mp3", "Song3.mp3"];
 const poster = ["Poster1.jpg", "Poster2.jpg", "Poster3.jpg"];
 
 const song = new Audio();
+let index = 0, temp;
 
-pausePlayBtn.forEach((btn, i) => {
-   btn.addEventListener('click', function () {
-      changePoster(i)
-      song.src = songs[i];
-      togglePlay(song)
+[...buttons].forEach((btn, i) => {
+   song.src = songs[index];
+
+   btn.addEventListener('click', () => {
+      temp = index
+      index = i
+      changePoster(i);
+
+      if (temp !== index) {
+         song.src = songs[index];
+         buttons[temp].firstElementChild.src = 'Play.png'
+      }
+
+      if (song.paused) {
+         song.play()
+         btn.firstElementChild.src = 'Pause.png'
+      } else {
+         song.pause()
+         btn.firstElementChild.src = 'Play.png'
+      }
+
    })
+
 })
 
 song.addEventListener('timeupdate', function () {
-
    const position = song.currentTime / song.duration;
-
    fillBar.style.width = position * 100 + '%';
 });
 
@@ -27,19 +44,3 @@ function changePoster(i) {
    bgImg.setAttribute('src', poster[i])
    songPoster.setAttribute('src', poster[i])
 }
-
-var isPlaying = false;
-
-function togglePlay(s) {
-   if (isPlaying) {
-      s.pause()
-   } else {
-      s.play();
-   }
-};
-song.onplaying = function () {
-   isPlaying = true;
-};
-song.onpause = function () {
-   isPlaying = false;
-};
